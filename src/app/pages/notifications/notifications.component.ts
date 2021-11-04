0e37af5c173dd12d2604a07979700d7a92e2305e
Jonatan <aguilarupch@hotmail.com>
2021-11-04 14:13:18 -0600

    cambios

--- src/app/pages/notifications/notifications.component.ts
+++ src/app/pages/notifications/notifications.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import PSPDFKit from 'pspdfkit';
-
 @Component({
   selector: 'notifications-cmp',
   moduleId: module.id,
@@ -8,21 +6,9 @@ import PSPDFKit from 'pspdfkit';
 })
 
 export class NotificationsComponent {
-
   constructor() { }
 
-  ngAfterViewInit(): void {
-    PSPDFKit.load({
-      baseUrl: location.protocol + "//" + location.host + "/assets/",
-      document: "assets/hojaGeneral.pdf",
-      container: "#pspdfkit-container",
-    }).then((instance) => {
-      // For the sake of this demo, store the PSPDFKit for Web instance
-      // on the global object so that you can open the dev tools and
-      // play with the PSPDFKit API.
+   ngOnInit(){
+  }
+}
 
-      (<any>window).instance = instance;
-    });
-    PSPDFKit.TextLine
-  }
-  }
